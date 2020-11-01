39640ef1b0a3cc190f2a29da96de5f4a14b9b401
Clinton Paiva <clinton@numadic.com>
2020-11-01 12:04:57 +0530

    postJob form
    form to post a job

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,65 @@
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
-import { Layout, Menu, Image, Button, Modal } from 'antd';
+import { Layout, Menu, Image, Form, Input, Button, Modal,Row, Col,Slider } from 'antd';
 import StartupGoaLogo from '../assets/StartupGoaLogo.png'
 import { Link } from 'react-router-dom';
-import * as React from 'react';
+import React, { useState ,useEffect} from 'react';
 
 const { Header } = Layout;
-const { useState, useEffect } = React;
 
 function Mainheader() {
   const [visible, setvisible] = useState(false);
   const [menukey, setmenukey] = useState('1');
+  const [form] = Form.useForm();
+  const [requiredMark, setRequiredMarkType] = useState(true);
 
+const marks = {
+  0: '1L',
+  5: '2L',
+  10: '3L',
+  15: '4L',
+  20: {
+    style: {
+      color: '#f50',
+    },
+    label: <strong>5L</strong>,
+  },
+  25: '6L',
+  30: '7L',
+  35: '8L',
+  40: '8L',
+  45: {
+    style: {
+      color: '#f50',
+    },
+    label: <strong>10L</strong>,
+  },
+  55: '11L',
+  65: '12L',
+  75: '13L',
+  85: '14L',
+  95: {
+    style: {
+      color: '#f50',
+    },
+    label: <strong>15L</strong>,
+  },
+};
+  const onRequiredTypeChange = ({ requiredMark }) => {
+    setRequiredMarkType(requiredMark);
+  };
   const showModal = () => {
     setvisible(true)
   };
   const handleOk = () => {
+    let payload={
+      title:form.getFieldValue('title'),
+      type:form.getFieldValue('type'),
+      location:form.getFieldValue('location'),
+      desc:form.getFieldValue('description'),
+      category:form.getFieldValue('category'),
+      salary:form.getFieldValue('salary'),
+    }
+    console.log(payload)
     setvisible(false)
   };
 
@@ -26,6 +71,7 @@ function Mainheader() {
     setmenukey(key.toString())
   }
   return (
+  //const { getFieldDecorator } = this.props.form;
 
     <Header>
 
@@ -66,20 +112,44 @@ function Mainheader() {
         title="Post a job"
         onOk={handleOk}
         onCancel={handleCancel}
-        footer={[
-          <Button key="back" onClick={handleCancel}>
-            Return
-            </Button>,
+        footer={
           <Button key="submit" type="primary" onClick={handleOk}>
             Submit
-            </Button>,
-        ]}
+            </Button>
+        }
       >
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
+        <Form
+      form={form}
+      layout="vertical"
+      initialValues={{ requiredMark }}
+      onValuesChange={onRequiredTypeChange}
+      requiredMark={requiredMark}
+    >
+    <Form.Item label="Job title" name="title" required >
+        <Input placeholder="What is the job / role" />
+      </Form.Item>
+      <Row gutter={8}>
+      <Col span={12}>
+       <Form.Item label="Job type" name="type" required >
+        <Input placeholder="Freelance, Full-time, Internship, Part-time" />
+      </Form.Item>
+      </Col>
+      <Col span={12}>
+      <Form.Item label="Location" name="location" required >
+        <Input placeholder="Arpora, Colva, Saligao.." />
+      </Form.Item>
+       </Col>
+        </Row>
+       <Form.Item label="Job category" name="category" required >
+        <Input placeholder="Development, management.." />
+      </Form.Item>
+       <Form.Item label="Job description" name="description" required >
+        <Input.TextArea placeholder="Give an overview of whar is the job about and role & responsibility" />
+      </Form.Item>
+      <Form.Item label="Salary range (per annum)" name="salary" required > 
+          <Slider marks={marks} step={10} defaultValue={5} /> 
+      </Form.Item> 
+    </Form>
       </Modal>
     </Header>
 
